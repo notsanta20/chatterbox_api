5f006c4cd028d1a4a4745dca1f70df7445ca3017
santhosh <santhosh.003z@gmail.com>
2025-04-22 23:08:12 +0530

    feat: added join and create group APIs

---
+++ controllers/joinGroup.js
@@ -0,0 +1,46 @@
+const { PrismaClient } = require("../prisma/generated/prisma/client");
+const prisma = new PrismaClient();
+
+async function joinGroup(req, res) {
+  let { groupId } = req.body;
+
+  if (req.authorization) {
+    try {
+      if (typeof groupName === "undefined") {
+        res
+          .status(401)
+          .json({
+            error: "Group Id must not be empty",
+            auth: req.authorization,
+          });
+        return;
+      }
+
+      const data = await prisma.groupMembers.create({
+        data: {
+          groupId: groupId,
+          userId: req.user.id,
+        },
+      });
+      console.log(data);
+
+      res.json({
+        message: "group created successfully",
+        auth: req.authorization,
+      });
+    } catch (error) {
+      res
+        .status(501)
+        .json({ error: "Internal server error", auth: req.authorization });
+    }
+  } else {
+    res
+      .status(401)
+      .json({
+        error: "Unauthorized entry, login to join group",
+        auth: req.authorization,
+      });
+  }
+}
+
+module.exports = joinGroup;
